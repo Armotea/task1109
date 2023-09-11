dcac64af73b1bc72273e3a929070e71f32a8aa28
Armotea <gong322@mail.ru>
2023-09-12 01:59:36 +0300

    Добавил комментарии.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-stroke = input("Введите строковое значение: ")
+stroke = input("Введите строковое значение: ")                  #Получение строки от пользователя
 print("Ваша строка: ", stroke)
 
-stroke = stroke.replace(" ", "")
-secure = list()
+stroke = stroke.replace(" ", "")                                #Удаление пробелов в строке
+secure = list()                                                 #Список для последующей проверки
 
 for i in range(len(stroke)):
-    if stroke[i] not in secure:
-        numb = stroke.count(stroke[i])
+    if stroke[i] not in secure:                                 #Проверка на начиличе символа в списке secure,
+        numb = stroke.count(stroke[i])                          #если символа нет в списке secure, то выводится кол-во вхождений символа в список stroke
         print("Кол-во букв ", stroke[i], " - ", numb)
-        secure.append(stroke[i])
+        secure.append(stroke[i])                                #К списку secure добавляется символ, прошедший через цикл
